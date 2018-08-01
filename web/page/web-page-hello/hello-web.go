4fb8b284407e09ce6f26470eef5807a21d13f7cd
bnaya eshet <bnayanet@hotmail.com>
2018-08-01 16:49:03 +0300

    mandatory query string

--- web/page/web-page-hello/hello-web.go
+++ web/page/web-page-hello/hello-web.go
@@ -13,7 +13,7 @@ func main() {
 	http.HandleFunc("/hello", handleRequests)
 	serveOn := "localhost:7777"
 	url := "http://" + serveOn + "/hello"
-	fmt.Println("Serving on ", url, " you are allowed to send query in the form of ?q=")
+	fmt.Println("Serving on ", url, " you should send query in the form of ?q=")
 	log.Fatal(http.ListenAndServe(serveOn, nil))
 }
 
@@ -22,6 +22,9 @@ func handleRequests(w http.ResponseWriter, req *http.Request) {
 	message := " no query"
 	if q != "" {
 		message = " query is [" + q + "]"
+	} else {
+		http.Error(w, "missing 'q' URL parameter", http.StatusBadRequest)
+		return
 	}
 	log.Println("serving", req.URL)
 	fmt.Fprint(w, "Hello ", message, " ", time.Now().Format("2006-01-02 15:04:05"))
